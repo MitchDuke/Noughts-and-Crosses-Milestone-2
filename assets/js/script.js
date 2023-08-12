ef647404f0e888205f02f33408dff6bd5eb6194f
MitchDuke <mdmsduke@googlemail.com>
2023-08-12 10:31:17 +0000

    added empty functions to setup the basis of the logic, the start, reset, detemine winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,59 @@ let currentPlayer = 'X';
 let player1Score = 0;
 let player2Score = 0;
 
+// DOM elements
+const cells = Array.from(document.getElementsByClassName('cell'));
+const player1ScoreElement = document.getElementById('player1Score');
+const player2ScoreElement = document.getElementById('player2Score');
+
+// click event listeners for cells
+
+cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => makeMove(index));
+});
+
+
+// Make a move in game
+
+function makeMove() {
+
+};
+
+// Switch between players
+
+function switchPlayer() {
+
+};
+
+//Check for winner function
+
+function checkWin() {
+
+};
+
+// Handle a win
+
+function handleWin(player) {
+
+};
+
+// Handle a draw
+
+function handleDraw() {
+
+};
+
+// Reset the game
+
+function resetGame() {
+
+};
+
+// Reset the board
+
+function resetBoard() {
+
+};
+
+// Setup a game
+resetBoard();
